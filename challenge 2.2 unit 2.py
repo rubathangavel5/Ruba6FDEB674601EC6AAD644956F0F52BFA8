60ec7e1147c555ea7cea756781b02a7b4532afb9
rubathangavel5 <144410023+rubathangavel5@users.noreply.github.com>
2023-09-16 18:44:53 +0530

    Add files via upload

---
+++ challenge 2.2 unit 2.py
@@ -0,0 +1,17 @@
+#class Batsman(player):
+    def play(self);
+        print("The batsman is batting. ") 
+
+#Define the derived class Bowler 
+class Bowler (player);
+    def play(self);
+        print(" the bowler is bowling.")
+
+  #create object of Batman and bowler classes
+  batsman=batsman()
+  bowler=bowler ()
+
+#call the play()method for each object
+  batsman. play ()
+  bowler. play()
+
